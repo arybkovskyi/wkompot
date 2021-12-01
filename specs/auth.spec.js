103ec13c13b1ef925cad0707aebefe437d1fb247
Alex Rybkovskyi <alex.rybkovskyi@gmail.com>
2021-12-01 01:10:08 -0600

    Refactor auth test with POM

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -1,14 +1,11 @@
+import LoginPage from '../pages/login.page'
+import ProfilePage from '../pages/profile.page'
+
 describe('Auth', function() {
     it('Successful log in', async function(){
-        await browser.url('/user/login');
-        await expect($('.login-form-button')).toBeDisabled();
-        await $('[qa-id="email"]')
-            .setValue('diwej38033@luxiu2.com');
-        await $('[qa-id="password"]')
-            .setValue('Qwerty123');
-        await expect($('.login-form-button')).toBeEnabled();
-        await $('.login-form-button').click();
-        await expect($('img[alt="avatarIcon"]')).toBeDisplayed();
-        await browser.pause('3000')
+        await LoginPage.open();
+        await expect(LoginPage.btnSubmit).toBeDisabled();
+        await LoginPage.login('diwej38033@luxiu2.com','Qwerty123');
+        await expect(ProfilePage.iconUser).toBeDisplayed();
     })
 });
